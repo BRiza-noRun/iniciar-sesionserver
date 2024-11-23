b6d056625d9bef363decfaee94452f2fc60613ef
Azul12AB <ruyhnaydahuillca@gmail.com>
2024-11-23 18:07:38 -0500

    Terminado

--- registrate.php
+++ registrate.php
@@ -1,17 +1,17 @@
-<?php session_start();
+<?php
+session_start();
 if (isset($_SESSION['usuario'])) {
     header('Location: index.php');
 }
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $usuario = filter_var(strtolower($_POST['usuario']), FILTER_SANITIZE_STRING);
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
 
-    //echo " $usuario . $password . $password2";
-
     $errores = '';
 
-    if (empty($usuario) or empty($password) or empty($password2)) {
+    if (empty($usuario) || empty($password) || empty($password2)) {
         $errores = '<li>Por favor rellena todos los datos correctamente</li>';
     } else {
         try {
@@ -19,13 +19,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         } catch (PDOException $e) {
             echo "Error:" . $e->getMessage();
         }
-        $statement = $conexion->prepare('SELECT * FROM usuarios WHERE usuario * :usuario LIMIT 1');
+
+        $statement = $conexion->prepare('SELECT * FROM usuarios WHERE usuario = :usuario LIMIT 1');
         $statement->execute(array(':usuario' => $usuario));
         $resultado = $statement->fetch();
 
         if ($resultado != false) {
             $errores .= '<li>El nombre de usuario ya existe</li>';
+        } else {
+
+            if ($password !== $password2) {
+                $errores .= '<li>Las contraseñas no coinciden</li>';
+            } else {
+
+                $statement = $conexion->prepare('INSERT INTO usuarios (usuario, password) VALUES (:usuario, :password)');
+                $statement->execute(array(':usuario' => $usuario, ':password' => password_hash($password, PASSWORD_DEFAULT)));
+
+
+                header('Location: login.php');
+                exit();
+            }
         }
     }
 }
+
 require 'views/registrate.view.php';
